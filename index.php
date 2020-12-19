f54eb3597be1578764cb8a011329c27499c58324
Tan Zhong Ming <mingblog0520@gmail.com>
2020-12-19 17:58:29 +0800

    Update index.php
    migrate class from index.php to class.inc.php
    
    modify clear and create report btn

--- index.php
+++ index.php
@@ -2,230 +2,12 @@
 session_start();
 include_once('dbh.inc.php');
 include_once('navbar.php');
-class Attendences
-{
-    public $aid;
-    public $did;
-    public $pid;
-    public $lid;
-    public $oid;
-    public $status;
-    public $fees;
-}
-
-class Dimension
-{
-    public $tblName;
-    public $pkey;
-    public $column = array();
-    
-    public function __construct($tblName, $pkey)
-    {
-        $this->tblName = $tblName;
-        $this->pkey    = $pkey;
-    }
-    
-    public function addColumn($key, $col)
-    {
-        $this->column[$key] = $col;
-    }
-    
-    public function getTblCol($key)
-    {
-        if (array_key_exists($key, $this->column)) {
-            // echo "Array Key exists...<br>";
-            return $this->tblName . "." . $this->column[$key];
-        } else {
-            // echo "Array Key does not exist...<br>";
-            return false;
-        }
-        return false;
-    }
-
-    public function printDimension(){
-        if(empty($this->column)){
-            print "<h1>Dimension column is empty!</h1>";
-            return false;
-        }
-        $cols = $this->column;
-        $tblName = $this->tblName;
-        foreach($cols as $col){
-            $value = $tblName."." . $col;
-            print("<option value='$value'>$value</option>");
-        }
-    }
-}
+include_once('class.inc.php');
 
 function print_pre($msg){
     print('<pre>');
     print_r($msg);
     print('</pre>');
-}
-
-class Measure
-{
-    public $name;
-    public $column;
-    public $operation;
-    public $formatString;
-}
-
-class Fact
-{
-    public $tblName;
-    public $pkey;
-    public $dimension;
-    public $measure;
-    public $reference;
-    public $agr = array('SUM', 'AVG', 'COUNT', 'MIN', 'MAX');
-
-    public function printAllMeasure(){
-        print("<h3>Measure</h3>");
-        foreach($this->measure as $key => $mea){ 
-            $self = $_SERVER['PHP_SELF'];
-            print("<form action='$self' method='post'>");
-            print('<label>'.ucfirst($mea->name).'</label>&nbsp;:&nbsp;');
-            print("<select name='measure' id='measure'>");
-            $this->printMeasure($key);
-            print('</select>');
-            print('<input type="submit" value="Add"> ');
-            print('</form>');
-        }
-    }
-
-    public function printAllDimension(){
-        print("<h3>Dimension</h3>");
-        foreach($this->dimension as $key => $dim){ 
-            $self = $_SERVER['PHP_SELF'];
-            print("<form action='$self' method='post'>");
-            print('<label>'.ucfirst($dim->tblName).'</label>&nbsp;:&nbsp;');
-            print("<select name='dimension' id='dimension'>");
-            $dim->printDimension();
-            print('</select>');
-            print('<input type="submit" value="Add"> ');
-            print('</form>');
-        }
-    }
-
-    public function addReference($key,$value){
-        $this->reference[$key] = $value;
-    }
-    
-    public function get_dimension($key)
-    {
-        if (array_key_exists($key, $this->dimension)) {
-            return $this->dimension[$key];
-        } else {
-            return false;
-        }
-    }
-    
-    public function printMeasure($key)
-    {
-        if (array_key_exists($key, $this->measure)) {
-            $mea = $this->measure[$key];
-            print_r($mea);
-            foreach ($this->agr as $op) {
-                $value = $op . "(" . $mea->column . ')';
-                print("<option value='$value'>$value</option>");
-            }
-        } else {
-            return false;
-        }
-    }
-    
-    public function getMeasure($key)
-    {
-        if (array_key_exists($key, $this->measure)) {
-            $mea = $this->measure[$key];
-            $tmp = $mea->operation . "(" . $mea->column . ")";
-            return $tmp;
-        } else
-            return false;
-    }
-    public function generateSQL()
-    {
-        // Initialize
-        $query = 'SELECT DISTINCT ';
-        $join = ' RIGHT JOIN ';
-        
-        // Set counter and length for measure array
-        $counter = 1;
-        $len     = count($_SESSION['measure']);
-        
-        // Adding measure
-        foreach ($_SESSION['measure'] as $measure) {
-            if ($measure != "") {
-                if ($counter != $len) {
-                    $query = $query . $measure . ',';
-                } else {
-                    $query = $query . $measure;
-                }
-                
-            }
-            $counter++;
-        }
-        // Set counter and length for dimension array
-        $counter = 1;
-        $len     = count($_SESSION['dimension']);
-        if ($len >= 1) {
-            $query = $query . ",";
-        }
-        // Adding dimension
-        foreach ($_SESSION['dimension'] as $dimension) {
-            if ($dimension != "") {
-                if ($counter != $len) {
-                    $query = $query . $dimension . ',';
-                } else {
-                    $query = $query . $dimension;
-                }
-                
-            }
-            $counter++;
-        }
-        
-        // Add fact table name
-        $query = $query . ' FROM ' . $this->tblName . ' ';
-        
-        $tables = array();
-
-        foreach ($_SESSION['dimension'] as $dimension){
-            $tables[] = explode('.',$dimension)[0];
-        }
-        // https://www.tutorialrepublic.com/faq/how-to-remove-empty-values-from-an-array-in-php.php#:~:text=You%20can%20simply%20use%20the,array%20using%20a%20callback%20function.
-        $tables = array_filter($tables);
-        // https://www.php.net/manual/en/function.array-unique.php
-        $uniques = array_unique($tables);
-
-        // Adding join query
-        foreach($uniques as $unique){
-            $query = $query .
-            $join. $unique. " ON " . 
-            $this->tblName . ".". $this->reference[$unique] . "=".
-            $unique."." .$this->dimension[$unique]->pkey
-            ;
-        }
-        $query = $query . ' GROUP BY ';
-
-        $_SESSION['dimension'] = array_filter($_SESSION['dimension']);
-        $counter = 1;
-        $len = count($_SESSION['dimension']);
-
-        foreach($_SESSION['dimension'] as $dimension){
-            if($counter != $len){
-                $query = $query . $dimension . ",";
-            }
-            else{
-                $query = $query . $dimension;
-            }
-            
-            $counter++;
-        }
-        // print_pre($uniques);
-        print_r("<hr>");
-        print_r($query);
-        return $query;
-    }
 }
 
 $datesTbl = new Dimension('dates', 'dateid');
@@ -288,6 +70,8 @@ $measure->formatString = '#,###.00';
 
 $attendences->measure['attendence'] = $measure;
 
+$_SESSION['attendences'] = serialize($attendences);
+
 if (isset($_POST['clear'])) {
     session_unset();
     $_SESSION['dimension'][] = "";
@@ -303,7 +87,10 @@ if (isset($_POST['measure'])) {
 
 if (isset($_POST['create'])) {
     $_SESSION['create'][] = $_POST['create'];
-    $attendences->generateSQL();
+    $query = $attendences->generateSQL($false);
+    $_SESSION['query'] = $query;
+    header("Location: report.php");
+    exit();
 }
 
 if (isset($_POST['deleteDimension'])) {
@@ -375,9 +162,7 @@ if (isset($_POST['deleteMeasure'])) {
                                 print($btn);
                                 print('</form>');
                             }
-                        }
-
-                        
+                        }  
                     ?>
                 </td>
 				<td>
@@ -386,15 +171,11 @@ if (isset($_POST['deleteMeasure'])) {
                             $attendences->printAllDimension() 
                         ?>
 
-					<form action="<?php
-echo $_SERVER['PHP_SELF'];
-?>" method="POST">
-						<button type="submit" name='clear' value='clear'>Clear</button>
+					<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+						<button type="submit" class = 'btn btn-warning' name='clear' value='clear'>Clear</button>
 					</form>
-					<form action="<?php
-echo $_SERVER['PHP_SELF'];
-?>" method="POST">
-						<button type="submit" name='create' value='create'>Create Report</button>
+					<form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+						<button type="submit" class = "btn btn-success"name='create' value='create'>Create Report</button>
 					</form>
 					<hr> </td>
 			</tr>
